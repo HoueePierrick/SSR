1d035a990fee0ee6a28cb156571c2e58a5db8513
Pierrick Houée <houee.pierrick@gmail.com>
2022-11-06 22:01:29 +0100

    finished

--- index.ts
+++ index.ts
@@ -16,7 +16,29 @@ app.get("/restaurants", async (req: any, res: any) => {
   const dom = new JSDOM(data);
   const obj = dom.window.document.querySelector("#__NEXT_DATA__").textContent;
   //   const {} = obj.props.initialState.home.feed.results.data;
-  res.json(JSON.parse(obj.props));
+  //   console.log(JSON.parse(obj).props);
+  res.json(
+    JSON.parse(obj).props.initialState.home.feed.results.data.filter(
+      (e: any) => e.typeName === "UILayoutList"
+    )
+  );
+});
+
+app.get("/restaurant", async (req: any, res: any) => {
+  const { data } = await axios.get(
+    "https://deliveroo.fr/fr/menu/Paris/10eme-gare-du-nord/paris-10e/?day=today&geohash=u09wjdrgxf0p&time=ASAP"
+  );
+  const dom = new JSDOM(data);
+  const obj = dom.window.document.querySelector("#__NEXT_DATA__").textContent;
+  //   const { restaurant, menu } = JSON.parse(obj);
+  const restaurant =
+    JSON.parse(obj).props.initialState.menuPage.menu.meta.restaurant;
+  const items = JSON.parse(obj).props.initialState.menuPage.menu.meta.items;
+  const categories =
+    JSON.parse(obj).props.initialState.menuPage.menu.meta.categories;
+
+  res.json({ restaurant, categories, items });
+  //   res.json(JSON.parse(obj).props.initialState.menuPage.menu);
 });
 
 app.listen(3210, () => {
