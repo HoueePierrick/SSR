149e00b72b49cf1c4024b530d0962e353c346a66
Pierrick Houée <houee.pierrick@gmail.com>
2022-11-06 19:03:46 +0100

    Bug at 17:15

--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
 const express = require("express");
-
 const app = express();
+const axios = require("axios");
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 app.get("/", (req: any, res: any) => {
   res.send("Welcome to my Deliveroo API");
+});
+
+app.get("/restaurants", async (req: any, res: any) => {
+  const { data } = await axios.get(
+    "https://deliveroo.fr/fr/restaurants/paris/paris-10eme-gare-de-lest?fulfillment_method=DELIVERY&geohash=u09wjdrgxfem"
+  );
+  const dom = new JSDOM(data);
+  const obj = dom.window.document.querySelector("#__NEXT_DATA__").textContent;
+  //   const {} = obj.props.initialState.home.feed.results.data;
+  res.json(JSON.parse(obj.props));
 });
 
 app.listen(3210, () => {
